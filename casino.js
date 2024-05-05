a70cb18d4a007a87662d70eeeded657475b9abf1
SaiMukhund <saimukhund2001@gmail.com>
2024-05-05 18:38:50 +0530

    added file for casino problem

---
+++ casino.js
@@ -0,0 +1,19 @@
+// 1. Deposit some money 
+// 2. Determine number of lines to bet on 
+// 3. collect a bet amount 
+// 4. spin the slot machine 
+// 5. check whether user won or not 
+// 6 .give user their winnings 
+// 7. repeat 
+
+
+// function deposit(){
+//     return 1
+// }
+const prompt = require("prompt-sync")() ;
+const deposit= () => {
+    const depositAmount=prompt("enter the deposit amount: ")
+    
+}
+
+deposit();
